676d31b7b1a9401ed67b4efc2dcfeb6518d1f441
Paul <pisaychuk@hortonworks.com>
2014-01-10 11:15:23 +0200

    refactored FeedReplicationS4

--- merlin/src/test/java/org/apache/falcon/regression/prism/FeedReplicationS4.java
+++ merlin/src/test/java/org/apache/falcon/regression/prism/FeedReplicationS4.java
@@ -21,65 +21,62 @@ package org.apache.falcon.regression.prism;
 import org.apache.falcon.regression.core.bundle.Bundle;
 import org.apache.falcon.regression.core.generated.feed.ActionType;
 import org.apache.falcon.regression.core.generated.feed.ClusterType;
-import org.apache.falcon.regression.core.helpers.ColoHelper;
-import org.apache.falcon.regression.core.helpers.PrismHelper;
 import org.apache.falcon.regression.core.util.InstanceUtil;
 import org.apache.falcon.regression.core.util.Util;
 import org.apache.falcon.regression.core.util.Util.URLS;
 import org.apache.falcon.regression.core.util.XmlUtil;
+import org.apache.falcon.regression.testHelper.BaseMultiClusterTests;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class FeedReplicationS4 {
+public class FeedReplicationS4 extends BaseMultiClusterTests {
 
-    PrismHelper prismHelper = new PrismHelper("prism.properties");
+    String baseTestHDFSDir = baseHDFSDir + "/FeedReplicationS4/${YEAR}/${MONTH}/${DAY}/${HOUR}";
+    String s4location = "s4://inmobi-iat-data/userplatform/${YEAR}/${MONTH}/${DAY}/${HOUR}";
+    Bundle b1 = new Bundle();
+    Bundle b2 = new Bundle();
 
-    ColoHelper ua1 = new ColoHelper("gs1001.config.properties");
-    ColoHelper ua2 = new ColoHelper("ivoryqa-1.config.properties");
+    @BeforeMethod(alwaysRun = true)
+    public void setupTest() throws Exception {
+        b1 = (Bundle) Bundle.readBundle("S4Replication")[0][0];
+        b1.generateUniqueBundle();
+        b2 = (Bundle) Bundle.readBundle("S4Replication")[0][0];
+        b2.generateUniqueBundle();
+        b1 = new Bundle(b1, server3.getEnvFileName());
+        b2 = new Bundle(b2, server2.getEnvFileName());
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        b1.deleteBundle(prism);
+    }
 
     @Test(enabled = true, timeOut = 1200000)
     public void ReplicationFromS4() throws Exception {
-
-        Bundle b1 = (Bundle) Bundle.readBundle("S4Replication")[0][0];
-        b1.generateUniqueBundle();
-
-        Bundle b2 = (Bundle) Bundle.readBundle("S4Replication")[0][0];
-        b2.generateUniqueBundle();
-
-
-        try {
-            b1 = new Bundle(b1, ua1.getEnvFileName());
-            b2 = new Bundle(b2, ua2.getEnvFileName());
-
-            //Bundle.submitCluster(b1,b2);
-
-            String feedOutput01 = b1.getDataSets().get(0);
-            feedOutput01 = InstanceUtil
-                    .setFeedCluster(feedOutput01,
-                            XmlUtil.createValidity("2010-10-01T12:00Z", "2099-01-01T00:00Z"),
-                            XmlUtil.createRtention("days(10000)", ActionType.DELETE), null,
-                            ClusterType.SOURCE, null,
-                            null);
-            feedOutput01 = InstanceUtil
-                    .setFeedCluster(feedOutput01,
-                            XmlUtil.createValidity("2012-12-06T05:00Z", "2099-10-01T12:10Z"),
-                            XmlUtil.createRtention("minutes(5)", ActionType.DELETE),
-                            Util.readClusterName(b2.getClusters().get(0)), ClusterType.SOURCE, "",
-                            "s4://inmobi-iat-data/userplatform/${YEAR}/${MONTH}/${DAY}/${HOUR}");
-            feedOutput01 = InstanceUtil
-                    .setFeedCluster(feedOutput01,
-                            XmlUtil.createValidity("2012-12-06T05:00Z", "2099-10-01T12:25Z"),
-                            XmlUtil.createRtention("minutes(5)", ActionType.DELETE),
-                            Util.readClusterName(b1.getClusters().get(0)), ClusterType.TARGET, "",
-                            "/replicationS4/${YEAR}/${MONTH}/${DAY}/${HOUR}");
-
-            System.out.println(feedOutput01);
-            Bundle.submitCluster(b1, b2);
-            prismHelper.getFeedHelper()
-                    .submitAndSchedule(URLS.SUBMIT_AND_SCHEDULE_URL, feedOutput01);
-
-        } finally {
-            b1.deleteBundle(prismHelper);
-        }
+        //Bundle.submitCluster(b1,b2);
+        String feedOutput01 = b1.getDataSets().get(0);
+        feedOutput01 = InstanceUtil
+                .setFeedCluster(feedOutput01,
+                        XmlUtil.createValidity("2010-10-01T12:00Z", "2099-01-01T00:00Z"),
+                        XmlUtil.createRtention("days(10000)", ActionType.DELETE), null,
+                        ClusterType.SOURCE, null,
+                        null);
+        feedOutput01 = InstanceUtil
+                .setFeedCluster(feedOutput01,
+                        XmlUtil.createValidity("2012-12-06T05:00Z", "2099-10-01T12:10Z"),
+                        XmlUtil.createRtention("minutes(5)", ActionType.DELETE),
+                        Util.readClusterName(b2.getClusters().get(0)), ClusterType.SOURCE, "",
+                        s4location);
+        feedOutput01 = InstanceUtil
+                .setFeedCluster(feedOutput01,
+                        XmlUtil.createValidity("2012-12-06T05:00Z", "2099-10-01T12:25Z"),
+                        XmlUtil.createRtention("minutes(5)", ActionType.DELETE),
+                        Util.readClusterName(b1.getClusters().get(0)), ClusterType.TARGET, "",
+                        baseTestHDFSDir);
+        System.out.println(feedOutput01);
+        Bundle.submitCluster(b1, b2);
+        prism.getFeedHelper()
+                .submitAndSchedule(URLS.SUBMIT_AND_SCHEDULE_URL, feedOutput01);
     }
-
 }
