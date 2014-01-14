c734734a1d979222d7dd1682613ac3cfbea3a133
Ruslan Ostafiychuk <rostafiychuk@hortonworks.com>
2014-01-14 19:23:41 +0200

    RetentionTest refactoring

--- merlin/src/test/java/org/apache/falcon/regression/prism/RetentionTest.java
+++ merlin/src/test/java/org/apache/falcon/regression/prism/RetentionTest.java
@@ -16,24 +16,18 @@
  * limitations under the License.
  */
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.apache.falcon.regression.prism;
 
 
 import org.apache.falcon.regression.core.bundle.Bundle;
 import org.apache.falcon.regression.core.generated.feed.Feed;
+import org.apache.falcon.regression.core.generated.feed.Location;
 import org.apache.falcon.regression.core.generated.feed.LocationType;
 import org.apache.falcon.regression.core.helpers.ColoHelper;
-import org.apache.falcon.regression.core.helpers.PrismHelper;
-import org.apache.falcon.regression.core.supportClasses.Consumer;
 import org.apache.falcon.regression.core.util.Util;
 import org.apache.falcon.regression.core.util.Util.URLS;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-import org.testng.TestNGException;
+import org.apache.falcon.regression.testHelper.BaseSingleClusterTests;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -44,120 +38,46 @@ import java.io.StringWriter;
 import java.lang.reflect.Method;
 import java.util.Random;
 
-public class RetentionTest {
-    //	@BeforeClass(alwaysRun=true)
-    public void createTestData() throws Exception {
-        Util.restartService(UA1coloHelper.getProcessHelper());
-        Util.restartService(UA2ColoHelper.getProcessHelper());
-        Util.restartService(UA3ColoHelper.getProcessHelper());
-    }
+public class RetentionTest extends BaseSingleClusterTests {
+
+    private Bundle bundle;
 
     @BeforeMethod(alwaysRun = true)
     public void testName(Method method) {
         Util.print("test name: " + method.getName());
     }
 
-
-    Consumer consumer;
-
-    PrismHelper prismHelper = new PrismHelper("prism.properties");
-    ColoHelper UA1coloHelper = new ColoHelper("mk-qa.config.properties");
-    ColoHelper UA2ColoHelper = new ColoHelper("gs1001.config.properties");
-    ColoHelper UA3ColoHelper = new ColoHelper("ivoryqa-1.config.properties");
-
-
-    DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy/MM/dd/HH/mm");
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        finalCheck(bundle);
+    }
 
     @Test(groups = {"0.1", "0.2", "prism"}, dataProvider = "betterDP", priority = -1)
-    public void testRetention(Bundle bundle, String period, String unit, boolean gaps,
-                              String dataType)
-    throws Exception {
-        Bundle ua2Bundle = new Bundle(bundle, UA2ColoHelper);
-        try {
+    public void testRetention(Bundle b, String period, String unit, boolean gaps, String dataType) throws Exception {
+        bundle = new Bundle(b, server1);
+        displayDetails(period, unit, gaps, dataType);
 
-            displayDetails(period, unit, gaps, dataType);
+        System.setProperty("java.security.krb5.realm", "");
+        System.setProperty("java.security.krb5.kdc", "");
+        String feed = setFeedPathValue(Util.getInputFeedFromBundle(bundle), getFeedPathValue(dataType));
+        feed = Util.insertRetentionValueInFeed(feed, unit + "(" + period + ")");
+        bundle.getDataSets().remove(Util.getInputFeedFromBundle(bundle));
+        bundle.getDataSets().add(feed);
+        bundle.generateUniqueBundle();
 
+        bundle.submitClusters(prism);
 
-            System.setProperty("java.security.krb5.realm", "");
-            System.setProperty("java.security.krb5.kdc", "");
-            String feed = setFeedPathValue(Util.getInputFeedFromBundle(ua2Bundle),
-                    getFeedPathValue(dataType));
-            feed = Util.insertRetentionValueInFeed(feed, unit + "(" + period + ")");
-            ua2Bundle.getDataSets().remove(Util.getInputFeedFromBundle(ua2Bundle));
-            ua2Bundle.getDataSets().add(feed);
-            ua2Bundle.generateUniqueBundle();
+        if (Integer.parseInt(period) > 0) {
+            Util.assertSucceeded(prism.getFeedHelper()
+                    .submitEntity(URLS.SUBMIT_URL, Util.getInputFeedFromBundle(bundle)));
 
-            ua2Bundle.submitClusters(prismHelper);
+            replenishData(server1, dataType, gaps);
 
-            if (Integer.parseInt(period) > 0) {
-                Util.assertSucceeded(prismHelper.getFeedHelper()
-                        .submitEntity(URLS.SUBMIT_URL, Util.getInputFeedFromBundle(ua2Bundle)));
-
-                replenishData(UA2ColoHelper, dataType, gaps);
-
-                Util.CommonDataRetentionWorkflow(UA2ColoHelper, ua2Bundle, Integer.parseInt(period),
-                        unit);
-            } else {
-                Util.assertFailed(prismHelper.getFeedHelper()
-                        .submitEntity(URLS.SUBMIT_URL, Util.getInputFeedFromBundle(ua2Bundle)));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new TestNGException(e.getCause());
-        } finally {
-
-            finalCheck(ua2Bundle);
+            Util.CommonDataRetentionWorkflow(server1, bundle, Integer.parseInt(period),unit);
+        } else {
+            Util.assertFailed(prism.getFeedHelper()
+                    .submitEntity(URLS.SUBMIT_URL, Util.getInputFeedFromBundle(bundle)));
         }
-    }
-
-
-    @DataProvider(name = "betterDP")
-    public Object[][] getTestData(Method m) throws Exception {
-        Bundle[] bundles = Util.getBundleData("RetentionBundles/valid/bundle1");
-        String[] units = new String[]{"hours", "days"};// "minutes","hours","days",
-        String[] periods = new String[]{"0", "10080", "60", "8",
-                "24"};// "0","10080","60","1","24"  //not using a negative value like -4 since
-                // the system does not
-        // allow setting it. Should be covered in validation scenarios.
-        Boolean[] gaps = new Boolean[]{false, true}; //,true
-        String[] dataTypes = new String[]{"daily", "yearly", "monthly"}; //
-
-/*		Object[][] testData=new Object[1][5];
-        testData[0][0]=bundles[0];
-		testData[0][1]=periods[1];
-		testData[0][2]=units[0];
-		testData[0][3]=gaps[1];
-		testData[0][4]=dataTypes[1]; 
-*/
-        Object[][] testData =
-                new Object[bundles.length * units.length * periods.length * gaps.length *
-                        dataTypes.length][5];
-
-        int i = 0;
-
-        for (int b = 0; b < bundles.length; b++) {
-            for (int u = 0; u < units.length; u++) {
-                for (int p = 0; p < periods.length; p++) {
-                    for (int g = 0; g < gaps.length; g++) {
-                        for (int d = 0; d < dataTypes.length; d++) {
-                            testData[i][0] = bundles[b];
-                            testData[i][1] = periods[p];
-                            testData[i][2] = units[u];
-                            testData[i][3] = gaps[g];
-                            testData[i][4] = dataTypes[d];
-                            i++;
-                        }
-                    }
-                }
-            }
-        }
-
-        return testData;
-    }
-
-    @DataProvider(name = "DP")
-    public Object[][] getBundles(Method m) throws Exception {
-        return Util.readBundles("RetentionBundles/valid/bundle1");
     }
 
     private String setFeedPathValue(String feed, String pathValue) throws Exception {
@@ -165,9 +85,7 @@ public class RetentionTest {
         Feed feedObject = (Feed) feedContext.createUnmarshaller().unmarshal(new StringReader(feed));
 
         //set the value
-        for (org.apache.falcon.regression.core.generated.feed.Location location : feedObject
-                .getLocations()
-                .getLocation()) {
+        for (Location location : feedObject.getLocations().getLocation()) {
             if (location.getType().equals(LocationType.DATA)) {
                 location.setPath(pathValue);
             }
@@ -179,8 +97,8 @@ public class RetentionTest {
     }
 
     private void finalCheck(Bundle bundle) throws Exception {
-        prismHelper.getFeedHelper().delete(URLS.DELETE_URL, Util.getInputFeedFromBundle(bundle));
-        Util.verifyFeedDeletion(Util.getInputFeedFromBundle(bundle), UA2ColoHelper);
+        prism.getFeedHelper().delete(URLS.DELETE_URL, Util.getInputFeedFromBundle(bundle));
+        Util.verifyFeedDeletion(Util.getInputFeedFromBundle(bundle), server1);
     }
 
     private void displayDetails(String period, String unit, boolean gaps, String dataType)
@@ -193,8 +111,7 @@ public class RetentionTest {
         System.out.println("***********************************************");
     }
 
-    private void replenishData(ColoHelper helper, String dataType, boolean gap)
-    throws Exception {
+    private void replenishData(ColoHelper helper, String dataType, boolean gap) throws Exception {
         int skip = 0;
 
         if (gap) {
@@ -203,8 +120,7 @@ public class RetentionTest {
         }
 
         if (dataType.equalsIgnoreCase("daily")) {
-            Util.replenishData(helper,
-                    Util.convertDatesToFolders(Util.getDailyDatesOnEitherSide(36, skip), skip));
+            Util.replenishData(helper, Util.convertDatesToFolders(Util.getDailyDatesOnEitherSide(36, skip), skip));
         } else if (dataType.equalsIgnoreCase("yearly")) {
             Util.replenishData(helper, Util.getYearlyDatesOnEitherSide(10, skip));
         } else if (dataType.equalsIgnoreCase("monthly")) {
@@ -226,5 +142,36 @@ public class RetentionTest {
     }
 
     final static int[] gaps = new int[]{2, 4, 5, 1};
+
+    @DataProvider(name = "betterDP")
+    public Object[][] getTestData(Method m) throws Exception {
+        Bundle[] bundles = Util.getBundleData("RetentionBundles/valid/bundle1");
+        String[] units = new String[]{"hours", "days"};// "minutes","hours","days",
+        String[] periods = new String[]{"0", "10080", "60", "8", "24"}; // a negative value like -4 should be covered in validation scenarios.
+        boolean[] gaps = new boolean[]{false, true};
+        String[] dataTypes = new String[]{"daily", "yearly", "monthly"};
+        Object[][] testData = new Object[bundles.length * units.length * periods.length * gaps.length * dataTypes.length][5];
+
+        int i = 0;
+
+        for (Bundle bundle : bundles) {
+            for (String unit : units) {
+                for (String period : periods) {
+                    for (boolean gap : gaps) {
+                        for (String dataType : dataTypes) {
+                            testData[i][0] = bundle;
+                            testData[i][1] = period;
+                            testData[i][2] = unit;
+                            testData[i][3] = gap;
+                            testData[i][4] = dataType;
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return testData;
+    }
 
 }
