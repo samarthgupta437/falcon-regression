b2c258141c544ecb120ac9b7739e236b758250ea
karishma <karishma.gulati@inmobi.com>
2014-02-28 09:21:29 +0000

    generate data funcs

--- merlin-core/src/main/java/org/apache/falcon/regression/Entities/FeedMerlin.java
+++ merlin-core/src/main/java/org/apache/falcon/regression/Entities/FeedMerlin.java
@@ -19,14 +19,18 @@
 package org.apache.falcon.regression.Entities;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.apache.falcon.regression.core.generated.feed.Cluster;
-import org.apache.falcon.regression.core.generated.feed.ClusterType;
-import org.apache.falcon.regression.core.generated.feed.Feed;
+import org.apache.falcon.regression.core.generated.feed.*;
+import org.apache.falcon.regression.core.util.HCatUtil;
+import org.apache.falcon.regression.core.util.HadoopUtil;
 import org.apache.falcon.regression.core.util.InstanceUtil;
+import org.apache.falcon.regression.core.util.Util;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hive.hcatalog.api.HCatClient;
 
 import javax.xml.bind.JAXBException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
 public class FeedMerlin extends org.apache.falcon.regression.core.generated
   .feed.Feed {
@@ -57,4 +61,76 @@ public class FeedMerlin extends org.apache.falcon.regression.core.generated
 
     return "";
   }
+
+    public void generateData(HCatClient cli, FileSystem fs){
+        String dataType="";
+        String loc="";
+        ArrayList<String> dataFolder;
+        String ur = element.getTable().getUri();
+        if(ur.indexOf(";")!= -1){
+            String[] parts = ur.split("#")[1].split(";");
+            int len=parts.length;
+            if(len==5) dataType="minutely";
+            else if(len==4) dataType="hourly";
+            else if(len==3) dataType="daily";
+            else if(len==2) dataType="monthly";
+        }else dataType="yearly";
+
+        String dbName=ur.split("#")[0].split(":")[1];
+        String tableName=ur.split("#")[0].split(":")[2];
+        try{
+            loc = cli.getTable(dbName,tableName).getLocation();
+            loc=loc+"/";
+
+            dataFolder=createData(fs, dataType, loc);
+            HCatUtil.createHCatTestData(cli, fs, dataType, dbName, tableName, dataFolder, loc);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void generateData(FileSystem fs){
+        String dataType="";
+        String pathValue="";
+        String loc = "";
+        for (Location location : element.getLocations().getLocation()) {
+            if (location.getType().equals(LocationType.DATA)) {
+                pathValue=location.getPath();
+            }
+        }
+
+        if(pathValue.contains("${MONTH}")){
+            String[] parts = pathValue.split("YEAR")[1].split("/");
+            int len=parts.length;
+            if(len==5) dataType="minutely";
+            else if(len==4) dataType="hourly";
+            else if(len==3) dataType="daily";
+            else if(len==2) dataType="monthly";
+        }else dataType="yearly";
+
+        loc = pathValue.substring(0,pathValue.indexOf("$"));
+        createData(fs, dataType, loc);
+    }
+
+    public ArrayList<String> createData(FileSystem fs, String dataType, String loc){
+        ArrayList<String> dataFolder = new ArrayList<String>();
+        try{
+            if (dataType.equalsIgnoreCase("daily")) {
+
+                dataFolder = HadoopUtil.createTestDataInHDFS(fs, Util.getDailyDatesOnEitherSide(36, 0), loc);
+            } else if (dataType.equalsIgnoreCase("yearly")) {
+                dataFolder = HadoopUtil.createTestDataInHDFS(fs,Util.getYearlyDatesOnEitherSide(10, 0), loc );
+            } else if (dataType.equalsIgnoreCase("monthly")) {
+                dataFolder = HadoopUtil.createTestDataInHDFS(fs,Util.getMonthlyDatesOnEitherSide(30, 0), loc );
+            } else if (dataType.equalsIgnoreCase("hourly")) {
+                dataFolder = HadoopUtil.createTestDataInHDFS(fs,Util.getHourlyDatesOnEitherSide(40, 0), loc );
+            } else if (dataType.equalsIgnoreCase("minutely")) {
+                dataFolder = HadoopUtil.createTestDataInHDFS(fs,Util.getMinuteDatesOnEitherSide(20, 0), loc );
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return dataFolder;
+
+    }
 }
