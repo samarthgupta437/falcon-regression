398f7a62b77914e6f811e27705841d70550dc0d3
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-03-06 15:13:13 -0800

    Fixing ProcessPartitionExpVariableTest.

--- merlin/src/test/java/org/apache/falcon/regression/prism/ProcessPartitionExpVariableTest.java
+++ merlin/src/test/java/org/apache/falcon/regression/prism/ProcessPartitionExpVariableTest.java
@@ -29,17 +29,25 @@ import org.apache.falcon.regression.testHelper.BaseTestClass;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.oozie.client.CoordinatorAction;
 import org.apache.oozie.client.OozieClient;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
 @Test(groups = "embedded")
 public class ProcessPartitionExpVariableTest extends BaseTestClass {
+    static Logger logger = Logger.getLogger(ProcessPartitionExpVariableTest.class);
 
     ColoHelper cluster1;
     FileSystem cluster1FS;
@@ -96,7 +104,7 @@ public class ProcessPartitionExpVariableTest extends BaseTestClass {
 
         Util.print(bundle.getProcessData());
 
-        InstanceUtil.createDataWithinDatesAndPrefix(cluster1,
+        createDataWithinDatesAndPrefix(cluster1,
                 InstanceUtil.oozieDateToDate(InstanceUtil.addMinsToTime(startTime, -25)),
                 InstanceUtil.oozieDateToDate(InstanceUtil.addMinsToTime(endTime, 25)),
                 baseTestDir + "/input1/", 1);
@@ -107,6 +115,46 @@ public class ProcessPartitionExpVariableTest extends BaseTestClass {
         InstanceUtil.waitTillInstanceReachState(cluster1OC,
                 Util.getProcessName(bundle.getProcessData()), 2,
                 CoordinatorAction.Status.SUCCEEDED, 20, ENTITY_TYPE.PROCESS);
+    }
+
+    private static void createDataWithinDatesAndPrefix(ColoHelper colo, DateTime startDateJoda,
+                                                      DateTime endDateJoda, String prefix,
+                                                      int interval) throws IOException, InterruptedException {
+        List<String> dataDates =
+                generateDateAndOneDayAfter(startDateJoda, endDateJoda, interval);
+
+        for (int i = 0; i < dataDates.size(); i++)
+            dataDates.set(i, prefix + dataDates.get(i));
+
+        List<String> dataFolder = new ArrayList<String>();
+
+        for (String dataDate : dataDates) dataFolder.add(dataDate);
+
+        InstanceUtil.putDataInFolders(colo, dataFolder,"");
+
+    }
+
+    private static List<String> generateDateAndOneDayAfter(DateTime startDate, DateTime endDate, int minuteSkip) {
+        //we want to generate patterns of the form .../2014/03/06/21/57/2014-Mar-07
+        //note there are two dates and the second date is one day after the first one
+        final DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy/MM/dd/HH/mm/");
+        final DateTimeFormatter formatter2 = DateTimeFormat.forPattern("yyyy-MMM-dd");
+        logger.info("generating data between " + formatter.print(startDate) + " and " +
+                formatter.print(endDate));
+
+        List<String> dates = new ArrayList<String>();
+
+
+        while (!startDate.isAfter(endDate)) {
+            final DateTime nextDate = startDate.plusMinutes(minuteSkip);
+            dates.add(formatter.print(nextDate) + formatter2.print(nextDate.plusDays(1)));
+            if (minuteSkip == 0) {
+                minuteSkip = 1;
+            }
+            startDate = nextDate;
+        }
+
+        return dates;
     }
 
     //TODO: ProcessPartitionExpVariableTest_OptionalPartition()
