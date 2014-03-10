61d8374e768bcff72d0493b766be9909b1d48c36
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-03-10 11:53:13 -0700

    Changing rest of the calls to take username as parameter.

--- merlin-core/src/main/java/org/apache/falcon/regression/core/util/Util.java
+++ merlin-core/src/main/java/org/apache/falcon/regression/core/util/Util.java
@@ -79,10 +79,8 @@ public class Util {
         return sendRequest(url, method, null, null);
     }
 
-    public static ServiceResponse sendRequest(String url, String method,
-                                              String data) throws IOException, URISyntaxException,
-    AuthenticationException{
-        return sendRequest(url, method, data, null);
+    public static ServiceResponse sendRequest(String url, String method, String user) throws IOException, URISyntaxException, AuthenticationException{
+        return sendRequest(url, method, null, user);
     }
 
     public static ServiceResponse sendRequest(String url, String method, String data,
