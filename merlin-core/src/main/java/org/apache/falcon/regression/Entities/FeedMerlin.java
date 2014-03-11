ee7768ae599e5474a22128fe288c38ee175082f2
karishma <karishma.gulati@inmobi.com>
2014-03-11 14:15:04 +0000

    added functions to FeedMerlin

--- merlin-core/src/main/java/org/apache/falcon/regression/Entities/FeedMerlin.java
+++ merlin-core/src/main/java/org/apache/falcon/regression/Entities/FeedMerlin.java
@@ -19,6 +19,8 @@
 package org.apache.falcon.regression.Entities;
 
 import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.falcon.regression.core.enums.FEED_TYPE;
+import org.apache.falcon.regression.core.generated.dependencies.Frequency;
 import org.apache.falcon.regression.core.generated.feed.*;
 import org.apache.falcon.regression.core.util.HCatUtil;
 import org.apache.falcon.regression.core.util.HadoopUtil;
@@ -28,7 +30,12 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hive.hcatalog.api.HCatClient;
 import org.joda.time.DateTime;
 
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.text.Format;
@@ -39,34 +46,34 @@ import java.util.Date;
 public class FeedMerlin extends Feed {
   private Feed element;
 
-  public FeedMerlin(String entity) throws JAXBException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    element = InstanceUtil.getFeedElement(entity);
+    public FeedMerlin(String entity) throws JAXBException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+      element = InstanceUtil.getFeedElement(entity);
 
-    Field[] fields = Feed.class.getDeclaredFields();
-    for (Field fld : fields) {
-      System.out.println("current field: "+fld.getName());
-      if("acl".equals(fld.getName()))
-        continue;
-      PropertyUtils.setProperty(this, fld.getName(),
-        PropertyUtils.getProperty(element, fld.getName()));
+      Field[] fields = Feed.class.getDeclaredFields();
+      for (Field fld : fields) {
+        System.out.println("current field: "+fld.getName());
+        if("acl".equals(fld.getName()))
+          continue;
+          PropertyUtils.setProperty(this, fld.getName(),
+          PropertyUtils.getProperty(element, fld.getName()));
+      }
     }
-  }
 
     /*
     all Merlin specific operations
      */
-  public String getTargetCluster() {
+    public String getTargetCluster() {
 
-    for (Cluster c : getClusters().getCluster()) {
-      if (c.getType().equals(ClusterType.TARGET))
-        return c.getName();
+      for (Cluster c : getClusters().getCluster()) {
+        if (c.getType().equals(ClusterType.TARGET))
+          return c.getName();
+      }
+
+      return "";
     }
 
-    return "";
-  }
-
-    public void generateData(HCatClient cli, FileSystem fs)throws Exception{
-        String dataType;
+    public void generateData(HCatClient cli, FileSystem fs, boolean isEmpty)throws Exception{
+        FEED_TYPE dataType;
         ArrayList<String> dataFolder;
         String ur = element.getTable().getUri();
 
@@ -75,22 +82,22 @@ public class FeedMerlin extends Feed {
             int len=parts.length;
             dataType=getDataType(len);
         }else {
-            dataType="yearly";
+            dataType=FEED_TYPE.YEARLY;
         }
 
 
         String dbName=ur.split("#")[0].split(":")[1];
         String tableName=ur.split("#")[0].split(":")[2];
 
-            String loc = cli.getTable(dbName,tableName).getLocation();
-            loc=loc+"/";
+        String loc = cli.getTable(dbName,tableName).getLocation();
+        loc=loc+"/";
 
-            dataFolder = createTestData(fs , dataType, loc);
-            HCatUtil.createHCatTestData(cli, fs, dataType, dbName, tableName, dataFolder);
-        }
+        dataFolder = createTestData(fs , dataType, loc, isEmpty);
+        HCatUtil.createHCatTestData(cli, fs, dataType, dbName, tableName, dataFolder);
+    }
 
-    public void generateData(FileSystem fs)throws Exception{
-        String dataType;
+    public void generateData(FileSystem fs, boolean isEmpty)throws Exception{
+        FEED_TYPE dataType;
         String pathValue="";
         for (Location location : element.getLocations().getLocation()) {
             if (location.getType().equals(LocationType.DATA)) {
@@ -98,19 +105,19 @@ public class FeedMerlin extends Feed {
             }
         }
 
-        if(pathValue.contains("${MONTH}")){
-            String[] parts = pathValue.split("YEAR")[1].split("/");
-            int len=parts.length;
-            dataType=getDataType(len);
+        String[] parts = pathValue.split("/$");
+        int len=parts.length;
+        if(len!=2){
+            dataType=getDataType(len-1);
         }else {
-            dataType="yearly";
+            dataType=FEED_TYPE.YEARLY;
         }
 
         String loc = pathValue.substring(0,pathValue.indexOf("$"));
-        createTestData(fs, dataType, loc);
+        createTestData(fs, dataType, loc, isEmpty);
     }
 
-    public ArrayList<String> createTestData(FileSystem fs, String dataType, String loc)throws Exception{
+    public ArrayList<String> createTestData(FileSystem fs, FEED_TYPE dataType, String loc, boolean isEmpty)throws Exception{
         ArrayList<String> dataFolder = new ArrayList<String>();
 
         Date start = element.getClusters().getCluster().get(0).getValidity().getStart();
@@ -122,27 +129,48 @@ public class FeedMerlin extends Feed {
         DateTime startDateJoda = new DateTime(InstanceUtil.oozieDateToDate(startDate));
         DateTime endDateJoda = new DateTime(InstanceUtil.oozieDateToDate(endDate));
 
-        dataFolder = HadoopUtil.createTestDataInHDFS(fs, Util.getDatesOnEitherSide(startDateJoda,endDateJoda,dataType), loc);
+        dataFolder = HadoopUtil.createTestDataInHDFS(fs, Util.getDatesOnEitherSide(startDateJoda,endDateJoda,dataType), loc, isEmpty);
         return dataFolder;
     }
 
-    public String getDataType(int len){
-        String dataType="";
+
+    public FEED_TYPE getDataType(int len){
 
         if(len==5) {
-            dataType="minutely";
+            return FEED_TYPE.MINUTELY;
         }
         else if(len==4) {
-            dataType="hourly";
+            return FEED_TYPE.HOURLY;
         }
         else if(len==3){
-            dataType="daily";
+            return FEED_TYPE.DAILY;
         }
         else if(len==2) {
-            dataType="monthly";
+            return FEED_TYPE.MONTHLY;
+        }
+        return null;
+    }
+
+    public String insertRetentionValueInFeed(String feed, String retentionValue)
+            throws JAXBException {
+
+        //insert retentionclause
+        element.getClusters().getCluster().get(0).getRetention()
+                .setLimit(new Frequency(retentionValue));
+
+        for (org.apache.falcon.regression.core.generated.feed.Cluster cluster : element
+                .getClusters().getCluster()) {
+            cluster.getRetention().setLimit(new Frequency(retentionValue));
         }
 
-        return dataType;
+        return InstanceUtil.feedElementToString(element);
+    }
+
+    public String setTableValue(String feed, String pathValue, String dBName, String tableName) throws Exception {
+
+        element.getTable().setUri("catalog:"+dBName+":"+tableName+"#"+pathValue);
+        //set the value
+        return InstanceUtil.feedElementToString(element);
     }
 
 }
