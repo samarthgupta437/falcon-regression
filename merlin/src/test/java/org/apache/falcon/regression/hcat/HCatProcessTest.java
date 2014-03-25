7a178db92756b92eeb3f1a7b98799d28d6ac394d
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-03-25 10:17:22 -0700

    addressing review comments

--- merlin/src/test/java/org/apache/falcon/regression/hcat/HCatProcessTest.java
+++ merlin/src/test/java/org/apache/falcon/regression/hcat/HCatProcessTest.java
@@ -263,10 +263,6 @@ public class HCatProcessTest extends BaseTestClass {
         InstanceUtil.waitTillInstanceReachState(
                 clusterOC, bundles[0].getProcessName(), 1, CoordinatorAction.Status.SUCCEEDED, 5, ENTITY_TYPE.PROCESS);
 
-        List<Path> inputData = HadoopUtil
-                .getAllFilesRecursivelyHDFS(cluster, new Path(inputHDFSDir + "/" + dataDates.get(0)));
-        List<Path> outputData = HadoopUtil
-                .getAllFilesRecursivelyHDFS(cluster, new Path(outputHDFSDir + "/dt=" + dataDates.get(0)));
         final ContentSummary inputContentSummary =
                 clusterFS.getContentSummary(new Path(inputHDFSDir + "/" + dataDates.get(0)));
         final ContentSummary inputContentSummary2 =
