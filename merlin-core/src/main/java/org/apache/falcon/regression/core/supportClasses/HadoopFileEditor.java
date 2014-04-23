dd09ec8ee6f428a23b73672ce146d478bcc6ccba
Paul <pisaychuk@hortonworks.com>
2014-04-23 17:32:55 +0300

    code formatting

--- merlin-core/src/main/java/org/apache/falcon/regression/core/supportClasses/HadoopFileEditor.java
+++ merlin-core/src/main/java/org/apache/falcon/regression/core/supportClasses/HadoopFileEditor.java
@@ -34,79 +34,77 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HadoopFileEditor {
-  static Logger logger = Logger.getLogger(HadoopFileEditor.class);
-  FileSystem fs;
-  List<String> paths ;
-  List<String> files;
+    static Logger logger = Logger.getLogger(HadoopFileEditor.class);
+    FileSystem fs;
+    List<String> paths;
+    List<String> files;
 
-  public HadoopFileEditor(FileSystem fs) {
-    this.fs = fs ;
-    paths = new ArrayList<String>();
-    files = new ArrayList<String>();
-  }
+    public HadoopFileEditor(FileSystem fs) {
+        this.fs = fs;
+        paths = new ArrayList<String>();
+        files = new ArrayList<String>();
+    }
 
-  /*
-  method to edit a file present on HDFS. Path is the location on HDFS,
-  2nd param is the first instance of string after u want ur tesxt to be
-  inserted, 3rd param is the text u want to insert
-   */
-  public void edit(String path, String putAfterString, String toBeInserted) throws IOException {
-    paths.add(path);
-    String currentFile = Util.getFileNameFromPath(path);
-    files.add(currentFile);
-    FileUtils.deleteQuietly(new File(currentFile));
-    FileUtils.deleteQuietly(new File("."+currentFile+".crc"));
-    FileUtils.deleteQuietly(new File(currentFile+".bck"));
-    FileUtils.deleteQuietly(new File("tmp"));
+    /*
+    method to edit a file present on HDFS. Path is the location on HDFS,
+    2nd param is the first instance of string after u want ur tesxt to be
+    inserted, 3rd param is the text u want to insert
+     */
+    public void edit(String path, String putAfterString, String toBeInserted) throws IOException {
+        paths.add(path);
+        String currentFile = Util.getFileNameFromPath(path);
+        files.add(currentFile);
+        FileUtils.deleteQuietly(new File(currentFile));
+        FileUtils.deleteQuietly(new File("." + currentFile + ".crc"));
+        FileUtils.deleteQuietly(new File(currentFile + ".bck"));
+        FileUtils.deleteQuietly(new File("tmp"));
 
-     Path file = new Path(path);
-    //check if currentFile exists or not
-    if(fs.exists(file)){
-      fs.copyToLocalFile(file, new Path(currentFile));
-      FileUtils.copyFile(new File(currentFile), new File(currentFile + ".bck"));
-      BufferedWriter bufwriter = new BufferedWriter(new FileWriter
-        ("tmp"));
-      BufferedReader br = new BufferedReader(new FileReader(currentFile));
-      String line ;
-      boolean isInserted = false ;
-      while( (line = br.readLine()) != null) {
-        bufwriter.write(line);
-        bufwriter.write('\n');
-        if(line.contains(putAfterString) && !isInserted) {
-          bufwriter.write(toBeInserted);
-          isInserted = true ;
+        Path file = new Path(path);
+        //check if currentFile exists or not
+        if (fs.exists(file)) {
+            fs.copyToLocalFile(file, new Path(currentFile));
+            FileUtils.copyFile(new File(currentFile), new File(currentFile + ".bck"));
+            BufferedWriter bufwriter = new BufferedWriter(new FileWriter
+                    ("tmp"));
+            BufferedReader br = new BufferedReader(new FileReader(currentFile));
+            String line;
+            boolean isInserted = false;
+            while ((line = br.readLine()) != null) {
+                bufwriter.write(line);
+                bufwriter.write('\n');
+                if (line.contains(putAfterString) && !isInserted) {
+                    bufwriter.write(toBeInserted);
+                    isInserted = true;
+                }
+            }
+            br.close();
+            bufwriter.close();
+            FileUtils.deleteQuietly(new File(currentFile));
+            FileUtils.copyFile(new File("tmp"), new File(currentFile));
+            FileUtils.deleteQuietly(new File("tmp"));
+
+            fs.delete(file, false);
+            File crcFile = new File("." + currentFile + ".crc");
+            if (crcFile.exists())
+                crcFile.delete();
+            fs.copyFromLocalFile(new Path(currentFile), file);
+        } else {
+            logger.info("Nothing to do, " + currentFile + " does not exists");
         }
-
-      }
-      br.close();
-      bufwriter.close();
-      FileUtils.deleteQuietly(new File(currentFile));
-      FileUtils.copyFile(new File("tmp"), new File(currentFile));
-      FileUtils.deleteQuietly(new File("tmp"));
-
-      fs.delete(file,false);
-      File crcFile = new File("."+currentFile+".crc");
-      if(crcFile.exists())
-        crcFile.delete();
-      fs.copyFromLocalFile(new Path(currentFile),file);
     }
-    else {
-      logger.info("Nothing to do, " + currentFile + " does not exists");
-    }
-   }
 
-  /*
-  puts back the original file to HDFS that was editied by edit function
-   */
-  public void restore() throws IOException {
-    for(int i = 0 ; i < paths.size() ; i++) {
-      fs.delete(new Path(paths.get(i)),false);
-      FileUtils.deleteQuietly(new File(files.get(i)));
-      FileUtils.copyFile(new File(files.get(i)+".bck"),
-        new File(files.get(i)));
-      fs.copyFromLocalFile(new Path(files.get(i)),new Path(paths.get(i)));
-      FileUtils.deleteQuietly(new File(files.get(i)));
-      FileUtils.deleteQuietly(new File(files.get(i)+".bck"));
+    /*
+    puts back the original file to HDFS that was editied by edit function
+     */
+    public void restore() throws IOException {
+        for (int i = 0; i < paths.size(); i++) {
+            fs.delete(new Path(paths.get(i)), false);
+            FileUtils.deleteQuietly(new File(files.get(i)));
+            FileUtils.copyFile(new File(files.get(i) + ".bck"),
+                    new File(files.get(i)));
+            fs.copyFromLocalFile(new Path(files.get(i)), new Path(paths.get(i)));
+            FileUtils.deleteQuietly(new File(files.get(i)));
+            FileUtils.deleteQuietly(new File(files.get(i) + ".bck"));
+        }
     }
-  }
 }
