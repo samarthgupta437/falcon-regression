3a42013394d3cfcc64fc5f257c2f09f798a99b36
Paul <pisaychuk@hortonworks.com>
2014-05-26 21:10:49 +0300

    new methods for lineage testing added

--- merlin/src/main/java/org/apache/falcon/regression/ui/pages/ProcessPage.java
+++ merlin/src/main/java/org/apache/falcon/regression/ui/pages/ProcessPage.java
@@ -21,10 +21,125 @@ package org.apache.falcon.regression.ui.pages;
 import org.apache.falcon.entity.v0.process.Process;
 import org.apache.falcon.regression.core.enumsAndConstants.ENTITY_TYPE;
 import org.apache.falcon.regression.core.helpers.PrismHelper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ProcessPage extends EntityPage<Process> {
-    public ProcessPage(WebDriver driver, PrismHelper helper, String entityName)  {
+
+    public ProcessPage(WebDriver driver, PrismHelper helper, String entityName) {
         super(driver, helper, ENTITY_TYPE.PROCESS, Process.class, entityName);
+    }
+
+    private boolean isLineageOpened = false;
+
+    private static final String LINE_AGE_BUTTON_XPATH = "//a[contains(., " +
+        "'Lineage')]";
+    private static final String CLOSE_LINE_AGE_BUTTON_XPATH =
+        "//div[@class='modal-footer']/button" +
+            "[contains(., 'Close')]";
+    private static final String INSTANCES_PANEL_XPATH =
+        "//div[@id='panel-instance']//table/tbody/tr";
+    private static final String INSTANCE_NAME_XPATH = ".//td[1]";
+    private static final String VERTICES_BLOCKS_XPATH = "//*[name() = 'svg']/*[name()" +
+        "='g']//*[name() = 'g'][not(@class='lineage-link')]";
+    private static final String CIRCLE_XPATH = "//*[name() = 'circle']";
+    private static final String LINEAGE_GRAPH_XPATH = "//*[name()='svg'][@id='lineage-graph']";
+    private static final String LINEAGE_INFO_PANEL = "//div[@id='lineage-info-panel']";
+
+    /**
+     * @param nominalTime - particular instance of process, defined by it's start time
+     */
+    public void openLineage(String nominalTime) throws InterruptedException {
+        waitForElement(INSTANCES_PANEL_XPATH, 5);
+        List<WebElement> instances = driver.findElements(By.xpath(INSTANCES_PANEL_XPATH));
+        for (WebElement instance : instances) {
+            String instanceName = instance.findElement(By.xpath(INSTANCE_NAME_XPATH)).getText();
+            System.out.println("Working with instance: " + instanceName);
+            if (instanceName.trim().equals(nominalTime)) {
+                WebElement lineage = driver.findElement(By.xpath(LINE_AGE_BUTTON_XPATH));
+                if (lineage != null) {
+                    instance.findElement(By.xpath(LINE_AGE_BUTTON_XPATH)).click();
+                    waitForElement(LINEAGE_GRAPH_XPATH, 5);
+                    Thread.sleep(3000);
+                    isLineageOpened = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    public void closeLineage() {
+        if (isLineageOpened) {
+            WebElement close = driver.findElement(By.xpath(CLOSE_LINE_AGE_BUTTON_XPATH));
+            close.click();
+            isLineageOpened = false;
+        }
+    }
+
+    /**
+     * @return map with instances names and their nominal start time
+     */
+    public HashMap<String, List<String>> getAllVertices() {
+        HashMap<String, List<String>> map = null;
+        if (isLineageOpened) {
+            waitForElement(CLOSE_LINE_AGE_BUTTON_XPATH, 5);
+            List<WebElement> blocks = driver.findElements(By.xpath(VERTICES_BLOCKS_XPATH));
+            map = new HashMap<String, List<String>>();
+            for (WebElement block : blocks) {
+                String text = block.getText();
+                String[] separate = text.split("/");
+                String name = separate[0];
+                String nominalTime = separate[1];
+                if (map.containsKey(name)) {
+                    map.get(name).add(nominalTime);
+                } else {
+                    List<String> instances = new ArrayList<String>();
+                    instances.add(nominalTime);
+                    map.put(name, instances);
+                }
+            }
+        }
+        return map;
+    }
+
+    /**
+     * Vertex is defined by it's name and particular time of it's creation
+     *
+     * @param entityName
+     * @param nominalTime
+     */
+    public void clickOnVertex(String entityName, String nominalTime) throws InterruptedException {
+        if (isLineageOpened) {
+            String particularBlock =
+                String.format("[contains(., '%s/%s')]", entityName, nominalTime);
+            WebElement circle = driver.findElement(By.xpath(VERTICES_BLOCKS_XPATH +
+                particularBlock + CIRCLE_XPATH));
+            Actions builder = new Actions(driver);
+            builder.click(circle).build().perform();
+            Thread.sleep(500);
+        }
+    }
+
+    public HashMap<String, String> getPanelInfo() {
+        HashMap<String, String> map = null;
+        if (isLineageOpened) {
+            //check if vertex was clicked
+            waitForElement(LINEAGE_INFO_PANEL + "//div[@class='col-md-3']", 3);
+            List<WebElement> infoBlocks =
+                driver.findElements(By.xpath(LINEAGE_INFO_PANEL + "//div[@class='col-md-3']"));
+            map = new HashMap<String, String>();
+            for (WebElement infoBlock : infoBlocks) {
+                String text = infoBlock.getText();
+                String[] values = text.split("\n");
+                map.put(values[0], values[1]);
+            }
+        }
+        return map;
     }
 }
