48b11d04fd4b7a5cd7432f5a763d3cd8e5728fdf
Ruslan Ostafiychuk <rostafiychuk@hortonworks.com>
2014-06-18 16:44:02 +0300

    Point class from selenium framework

--- merlin/src/main/java/org/apache/falcon/regression/ui/pages/ProcessPage.java
+++ merlin/src/main/java/org/apache/falcon/regression/ui/pages/ProcessPage.java
@@ -27,8 +27,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.openqa.selenium.Point;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
