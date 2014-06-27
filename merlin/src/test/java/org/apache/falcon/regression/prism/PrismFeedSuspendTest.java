e9aee00a930fcd2f6ce34a5ecd3c0ea475c18ba2
Paul <pisaychuk@hortonworks.com>
2014-06-27 17:21:36 +0300

    javadoced and tagged

--- merlin/src/test/java/org/apache/falcon/regression/prism/PrismFeedSuspendTest.java
+++ merlin/src/test/java/org/apache/falcon/regression/prism/PrismFeedSuspendTest.java
@@ -67,7 +67,14 @@ public class PrismFeedSuspendTest extends BaseTestClass {
         removeBundles();
     }
 
-    @Test(groups = {"prism", "0.2"})
+    /**
+     * Run two feed on different clusters. Delete 1st feed and try to suspend it. Should fail.
+     * Check that 2nd feed is running on 2nd cluster. Delete it and try to suspend it too.
+     * Attempt should fail and both feeds should be killed.
+     *
+     * @throws Exception
+     */
+    @Test(groups = {"prism", "0.2", "embedded"})
     public void testSuspendDeletedFeedOnBothColos() throws Exception {
         bundles[0].submitAndScheduleFeed();
         bundles[1].submitAndScheduleFeed();
@@ -91,12 +98,17 @@ public class PrismFeedSuspendTest extends BaseTestClass {
         AssertUtil.checkStatus(cluster2OC, ENTITY_TYPE.FEED, bundles[1], Job.Status.KILLED);
     }
 
-    @Test(groups = {"prism", "0.2"})
+    /**
+     * Run two feeds on different clusters. Suspend feed and try to suspend it once more. Check
+     * that action succeeds and feed is suspended. Make the same for 2nd feed.
+     *
+     * @throws Exception
+     */
+    @Test(groups = {"prism", "0.2", "distributed"})
     public void testSuspendSuspendedFeedOnBothColos() throws Exception {
         //schedule using colohelpers
         bundles[0].submitAndScheduleFeedUsingColoHelper(cluster1);
         bundles[1].submitAndScheduleFeedUsingColoHelper(cluster2);
-
         for (int i = 0; i < 2; i++) {
             //suspend using prism
             AssertUtil.assertSucceeded(
@@ -107,7 +119,6 @@ public class PrismFeedSuspendTest extends BaseTestClass {
             AssertUtil.checkStatus(cluster1OC, ENTITY_TYPE.FEED, bundles[0], Job.Status.SUSPENDED);
             AssertUtil.checkStatus(cluster2OC, ENTITY_TYPE.FEED, bundles[1], Job.Status.RUNNING);
         }
-
         for (int i = 0; i < 2; i++) {
             //suspend on the other one
             AssertUtil.assertSucceeded(
@@ -119,8 +130,12 @@ public class PrismFeedSuspendTest extends BaseTestClass {
         }
     }
 
-
-    @Test()
+    /**
+     * Attempt to suspend nonexistent feed should fail through both prism and matching server.
+     *
+     * @throws Exception
+     */
+    @Test(groups = "embedded")
     public void testSuspendNonExistentFeedOnBothColos() throws Exception {
         AssertUtil.assertFailed(prism.getFeedHelper()
             .suspend(Util.URLS.SUSPEND_URL, bundles[0].getDataSets().get(0)));
@@ -133,7 +148,12 @@ public class PrismFeedSuspendTest extends BaseTestClass {
             .suspend(Util.URLS.SUSPEND_URL, bundles[0].getDataSets().get(0)));
     }
 
-    @Test()
+    /**
+     * Attempt to suspend non-running feed should fail through both prism and matching server.
+     *
+     * @throws Exception
+     */
+    @Test(groups = "embedded")
     public void testSuspendSubmittedFeedOnBothColos() throws Exception {
         bundles[0].submitFeed();
         bundles[1].submitFeed();
@@ -147,11 +167,16 @@ public class PrismFeedSuspendTest extends BaseTestClass {
             .suspend(Util.URLS.SUSPEND_URL, bundles[0].getDataSets().get(0)));
         AssertUtil.assertFailed(cluster2.getFeedHelper()
             .suspend(Util.URLS.SUSPEND_URL, bundles[1].getDataSets().get(0)));
-
-
     }
 
-    @Test(groups = {"prism", "0.2"})
+    /**
+     * Run two feeds on different clusters. Stop server on 1st cluster. Attempt to suspend feed on
+     * stopped server through prism should fail. Check that 2nd feed is running. Suspend it
+     * and check that it is suspended.
+     *
+     * @throws Exception
+     */
+    @Test(groups = {"prism", "0.2", "distributed"})
     public void testSuspendScheduledFeedOnBothColosWhen1ColoIsDown() throws Exception {
         try {
             //schedule using colohelpers
@@ -181,7 +206,14 @@ public class PrismFeedSuspendTest extends BaseTestClass {
         }
     }
 
-    @Test(groups = {"prism", "0.2"})
+    /**
+     * Run two feeds on different clusters. Delete 1st feed. Stop server on 1st cluster. Attempt
+     * to suspend deleted feed on stopped server should fail. Delete 2nd feed. Attempt
+     * to suspend deleted 2nd feed should also fail. Check that both feeds are killed.
+     *
+     * @throws Exception
+     */
+    @Test(groups = {"prism", "0.2", "distributed"})
     public void testSuspendDeletedFeedOnBothColosWhen1ColoIsDown() throws Exception {
         try {
             //schedule using colohelpers
@@ -193,7 +225,6 @@ public class PrismFeedSuspendTest extends BaseTestClass {
                 prism.getFeedHelper()
                     .delete(Util.URLS.DELETE_URL, bundles[0].getDataSets().get(0))
             );
-
             Util.shutDownService(cluster1.getFeedHelper());
 
             //suspend using prism
@@ -223,13 +254,19 @@ public class PrismFeedSuspendTest extends BaseTestClass {
         }
     }
 
-    @Test(groups = {"prism", "0.2"})
+    /**
+     * Run two feeds on different clusters. Suspend 1st feed and check that it suspended,
+     * and then stop server on its cluster. Attempt to suspend the same feed again should fail.
+     * Suspend 2nd feed and check that both feeds are suspended.
+     *
+     * @throws Exception
+     */
+    @Test(groups = {"prism", "0.2", "distributed"})
     public void testSuspendSuspendedFeedOnBothColosWhen1ColoIsDown() throws Exception {
         try {
             //schedule using colohelpers
             bundles[0].submitAndScheduleFeedUsingColoHelper(cluster1);
             bundles[1].submitAndScheduleFeedUsingColoHelper(cluster2);
-
 
             //suspend using prism
             AssertUtil.assertSucceeded(
@@ -246,14 +283,12 @@ public class PrismFeedSuspendTest extends BaseTestClass {
                 prism.getFeedHelper()
                     .suspend(Util.URLS.SUSPEND_URL, bundles[0].getDataSets().get(0))
             );
-
             //suspend on the other one
             AssertUtil.assertSucceeded(
                 prism.getFeedHelper()
                     .suspend(Util.URLS.SUSPEND_URL, bundles[1].getDataSets().get(0)));
             AssertUtil.checkStatus(cluster1OC, ENTITY_TYPE.FEED, bundles[0], Job.Status.SUSPENDED);
             AssertUtil.checkStatus(cluster2OC, ENTITY_TYPE.FEED, bundles[1], Job.Status.SUSPENDED);
-
         } catch (Exception e) {
             e.printStackTrace();
             throw new TestNGException(e.getCause());
@@ -262,12 +297,16 @@ public class PrismFeedSuspendTest extends BaseTestClass {
         }
     }
 
-    @Test(groups = {"prism", "0.2"})
+    /**
+     * Stop the 1st server. Attempt to suspend nonexistent feeds on both clusters should fail.
+     *
+     * @throws Exception
+     */
+    @Test(groups = {"prism", "0.2", "distributed"})
     public void testSuspendNonExistentFeedOnBothColosWhen1ColoIsDown()
         throws Exception {
         try {
             Util.shutDownService(cluster1.getFeedHelper());
-
             AssertUtil.assertFailed(
                 prism.getFeedHelper()
                     .suspend(Util.URLS.SUSPEND_URL, bundles[1].getDataSets().get(0))
@@ -275,7 +314,6 @@ public class PrismFeedSuspendTest extends BaseTestClass {
             AssertUtil.assertFailed(
                 prism.getFeedHelper()
                     .suspend(Util.URLS.SUSPEND_URL, bundles[0].getDataSets().get(0)));
-
             AssertUtil.assertFailed(
                 cluster2.getFeedHelper()
                     .suspend(Util.URLS.SUSPEND_URL, bundles[0].getDataSets().get(0)));
@@ -287,7 +325,13 @@ public class PrismFeedSuspendTest extends BaseTestClass {
         }
     }
 
-    @Test(groups = {"prism", "0.2"})
+    /**
+     * Submit two feeds. Stop the server on the 1st cluster. Attempts to suspend non-scheduled
+     * feeds on both clusters should fail through prism as well as through colohelper.
+     *
+     * @throws Exception
+     */
+    @Test(groups = {"prism", "0.2", "distributed"})
     public void testSuspendSubmittedFeedOnBothColosWhen1ColoIsDown() throws Exception {
         try {
             bundles[0].submitFeed();
@@ -302,7 +346,6 @@ public class PrismFeedSuspendTest extends BaseTestClass {
             AssertUtil.assertFailed(
                 prism.getFeedHelper()
                     .suspend(Util.URLS.SUSPEND_URL, bundles[1].getDataSets().get(0)));
-
 
             AssertUtil.assertFailed(
                 cluster2.getFeedHelper()
