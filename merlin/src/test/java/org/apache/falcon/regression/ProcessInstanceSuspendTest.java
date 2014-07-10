08dca75ec65b867475c848a978d9728175d3ae91
Paul <pisaychuk@hortonworks.com>
2014-07-10 11:23:44 +0300

    time reduces from ~27 min to ~6 min

--- merlin/src/test/java/org/apache/falcon/regression/ProcessInstanceSuspendTest.java
+++ merlin/src/test/java/org/apache/falcon/regression/ProcessInstanceSuspendTest.java
@@ -70,8 +70,8 @@ public class ProcessInstanceSuspendTest extends BaseTestClass {
 
         Bundle bundle = BundleUtil.readELBundle();
         bundle = new Bundle(bundle, cluster);
-        String startDate = "2010-01-01T20:00Z";
-        String endDate = "2010-01-03T01:04Z";
+        String startDate = "2010-01-01T23:40Z";
+        String endDate = "2010-01-02T01:40Z";
         bundle.setInputFeedDataPath(feedInputPath);
         String prefix = bundle.getFeedDataPathPrefix();
         HadoopUtil.deleteDirIfExists(prefix.substring(1), clusterFS);
