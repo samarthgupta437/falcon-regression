b6d0e9f9ffbc76ddc5754e451c7cc51e03b5ec95
Raghav Kumar Gautam <raghavgautam@gmail.com>
2014-07-10 10:55:58 -0700

    adding methods back as per review comments

--- merlin-core/src/main/java/org/apache/falcon/regression/Entities/ProcessMerlin.java
+++ merlin-core/src/main/java/org/apache/falcon/regression/Entities/ProcessMerlin.java
@@ -21,14 +21,25 @@ package org.apache.falcon.regression.Entities;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.falcon.entity.v0.EntityType;
+import org.apache.falcon.entity.v0.process.Input;
 import org.apache.falcon.entity.v0.process.Process;
 import org.apache.falcon.entity.v0.process.Properties;
 import org.apache.falcon.entity.v0.process.Property;
+import org.apache.falcon.regression.core.bundle.Bundle;
+import org.apache.falcon.regression.core.util.InstanceUtil;
+import org.apache.falcon.regression.core.util.TimeUtil;
+import org.apache.falcon.regression.core.util.Util;
+import org.apache.hadoop.fs.FileSystem;
 import org.testng.Assert;
 
 import javax.xml.bind.JAXBException;
 import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProcessMerlin extends Process {
     public ProcessMerlin(String processData) {
@@ -45,6 +56,38 @@ public class ProcessMerlin extends Process {
         } catch (NoSuchMethodException e) {
             Assert.fail("Can't create ClusterMerlin: " + ExceptionUtils.getStackTrace(e));
         }
+    }
+
+    public Bundle setFeedsToGenerateData(FileSystem fs, Bundle b) {
+        Date start = getClusters().getClusters().get(0).getValidity().getStart();
+        Format formatter = new SimpleDateFormat("yyyy'-'MM'-'dd'T'HH':'mm'Z'");
+        String startDate = formatter.format(start);
+        Date end = getClusters().getClusters().get(0).getValidity().getEnd();
+        String endDate = formatter.format(end);
+
+        Map<String, FeedMerlin> inpFeeds = getInputFeeds(b);
+        for (FeedMerlin feedElement : inpFeeds.values()) {
+            feedElement.getClusters().getClusters().get(0).getValidity()
+                .setStart(TimeUtil.oozieDateToDate(startDate).toDate());
+            feedElement.getClusters().getClusters().get(0).getValidity()
+                .setEnd(TimeUtil.oozieDateToDate(endDate).toDate());
+            InstanceUtil.writeFeedElement(b, feedElement, feedElement.getName());
+        }
+        return b;
+    }
+
+    public Map<String, FeedMerlin> getInputFeeds(Bundle b) {
+        Map<String, FeedMerlin> inpFeeds = new HashMap<String, FeedMerlin>();
+        for (Input input : getInputs().getInputs()) {
+            for (String feed : b.getDataSets()) {
+                if (Util.readDatasetName(feed).equalsIgnoreCase(input.getFeed())) {
+                    FeedMerlin feedO = new FeedMerlin(feed);
+                    inpFeeds.put(Util.readDatasetName(feed), feedO);
+                    break;
+                }
+            }
+        }
+        return inpFeeds;
     }
 
     public final void setProperty(String name, String value) {
